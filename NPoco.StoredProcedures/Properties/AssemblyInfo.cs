bea9c148d3c703b6edc39b50cc7d43bd3030cda5
Simon Colmer <simon_colmer@hotmail.com>
2014-04-21 22:50:47 +0200

    Updated for Nuget

--- NPoco.StoredProcedures/Properties/AssemblyInfo.cs
+++ NPoco.StoredProcedures/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NPoco.StoredProcedures")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Basic class for querying stored procedures")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("X-Volt Studios")]
 [assembly: AssemblyProduct("NPoco.StoredProcedures")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
